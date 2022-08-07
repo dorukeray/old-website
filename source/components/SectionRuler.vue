bb976dd934916e3db73a802c8ec92823de5d8c50
doruk eray <doruk@dorkodu.com>
2022-08-07 23:24:18 +0300

    new component: SectionRuler

---
+++ source/components/SectionRuler.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="SectionRuler"></div>
+</template>
+<style lang="scss" scoped>
+@import "scss/styles.scss";
+
+.SectionRuler {
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+  border-bottom: .25rem dashed $reference-color-gray-90;
+}
+</style>
