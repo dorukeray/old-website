9fd2410675e341aa60428e4318d61e92fba05e7d
doruk eray <doruk@dorkodu.com>
2022-08-26 19:58:07 +0300

    new component: CareerStep
    for personal website & portfolio, i need a list-like no-border-card component with time, location, title and work description

---
+++ components/CareerStep.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="CareerStep">
+    <p v-if="time" class="time">{{ time }}</p>
+    <p>
+      <span v-if="time" class="topic">{{ topic }}</span>
+      <span v-if="time" class="location">{{ location }}</span>
+    </p>
+    <p v-if="description">{{ description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { time, title, location, description }
+  = defineProps<{
+    time: string;
+    title: string;
+    location: string;
+    description: string;
+  }>();
+</script>
+
+<style lang="scss" scoped>
+@import "scss/styles.scss";
+
+.CareerStep {
+  display: flex;
+  flex-direction: column;
+  gap: .25rem;
+  margin: 1rem 0;
+
+  p {
+    font-size: 1.05rem;
+    margin: 0;
+  }
+
+  .time {
+    font-weight: 600;
+    color: $reference-color-gray-70;
+  }
+
+  .title {
+    font-weight: 700;
+    font-size: 1.25rem;
+    margin-right: 1rem;
+  }
+
+  .location {
+    font-weight: 500;
+    color: $reference-color-gray-60;
+  }
+
+  .description {}
+}
+</style>
