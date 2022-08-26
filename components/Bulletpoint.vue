e1f137880812f821e15fe01c9aaa4cf877bbf7b5
doruk eray <doruk@dorkodu.com>
2022-08-26 18:53:06 +0300

    new component: Bulletpoint
    used with a single emoji/character as icon, and the content. viewed as a list item, but just flex container for iconed-content.

---
+++ components/Bulletpoint.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="Bulletpoint">
+    <span>{{ icon }}</span>
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+let { icon } = defineProps<{
+  icon: string;
+}>();
+</script>
+
+<style lang="scss">
+$iconSize: 2.25rem;
+
+.Bulletpoint {
+  display: flex;
+  margin: 1.5rem .5rem;
+  gap: 1rem;
+
+  .content {
+    width: calc(100% - 4rem);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: .25rem;
+
+
+    * {
+      padding: 0 !important;
+      margin: 0 !important;
+      text-align: left;
+    }
+  }
+
+  span {
+    font-size: $iconSize  !important;
+    height: auto !important;
+    width: auto !important;
+    overflow: visible;
+
+    .icon {
+      width: $iconSize;
+      height: $iconSize;
+    }
+  }
+}
+</style>
