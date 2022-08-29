0a034b8e7f2ad33e70319c4f16b69e5db388950f
doruk eray <doruk@dorkodu.com>
2022-08-29 20:06:00 +0300

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,21 +12,7 @@ export default defineNuxtConfig({
   css: [
     'scss/styles.scss',
   ],
-  // modules
-  modules: ['@nuxtjs/color-mode'],
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode',
-  },
-  vite: {
-    define: {
-      'process.env.DEBUG': false,
-    },
-  },
+  typescript: {
+    shim: false
+  }
 })
