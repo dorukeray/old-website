8e1997d4b7c7fd9524f42775350000dd16d507ea
doruk eray <doruk@dorkodu.com>
2022-09-04 18:14:43 +0300

    add htaccess to public

--- pages/story.vue
+++ pages/story.vue
@@ -9,7 +9,7 @@ import {
   ManIcon,
   CodeIcon,
   UserIcon,
-  FriendsIcon,
+  WorldIcon,
   HeartIcon,
   CertificateIcon,
   QuestionMarkIcon,
@@ -58,23 +58,30 @@ definePageMeta({
 
     <article>
       <h2>hello, again. 👋🏻</h2>
-      <h4 style="#aabbcc">let's continue from where we left 🍃</h4>
-      <Bulletpoint icon="👁‍🗨">
-        <p style="font-size: 1.25rem; margin: 0;">
-          <span class="paragraph-tagline"> my life purpose...</span>
-          <br>liberate the human,
+      <h4 style="#aabbcc">let's continue from where we left.</h4>
+      <Bulletpoint icon="🍃">
+        <p style="font-size: 1.25rem; margin: 0; font-weight: 400;">
+          <span class="paragraph-tagline">my purpose;</span>
+          <br><b>liberate the human.</b>
+        </p>
+      </Bulletpoint>
+      <Bulletpoint icon="🚩">
+        <p style="font-size: 1.25rem; margin: 0; font-weight: 400;">
+          <span class="paragraph-tagline">my mission;</span>
+          <br>create a free and happy life experience, for everyone,
           <br>build the utopian dream of heaven on earth,
-          <br>create a free and happy life experience, for everyone.
+          <br><b>...with meaningful technology.</b>
         </p>
       </Bulletpoint>
       <SectionRuler />
     </article>
+
     <article>
       <h4>why?</h4>
       <p>
-        simple. i want to live a free, happy and meaningful life. a humane life that everyone deserves.
-        i believe this is a fundemental human right: a livable world & a life worth living.
-        so we need to come together, and create this utopic dream life for everyone.
+        simple. <u>i want to live a free, happy and meaningful life.</u>
+        <br>i believe this is a fundemental right; a livable world & a life worth living.
+        <br>so we need to come together, and create this utopic dream life for everyone.
       </p>
       <h4>how?</h4>
       <p>i am doruk, a high-school kid passionate in products, design and software.
@@ -103,14 +110,14 @@ definePageMeta({
         <DescriptiveList title="know thyself." message="you ask me these common questions. so here are my answers! ⬇️">
           <DescriptiveItem title="who?"
             description="thinker, leader, artist, inventor, designer, polymath, idol, revolutionist, entrepreneur.">
-            <!-- <UserIcon class="symbol" /> -->
+            <UserIcon class="symbol" />
           </DescriptiveItem>
           <DescriptiveItem title="what i love to do?"
             description="think, design, code, learn, build, socialize, talk, think, feel... any type of creative activity.">
             <HeartIcon class="symbol" />
           </DescriptiveItem>
           <DescriptiveItem title="for whom?" description="myself, and then everyone.">
-            <EmpathizeIcon class="symbol" />
+            <WorldIcon class="symbol" />
           </DescriptiveItem>
           <DescriptiveItem title="why?"
             description="this is my mission, purpose of life. i always experience the human authenticity, being in the flow and losing sense of time and space.">
@@ -128,30 +135,43 @@ definePageMeta({
     <article>
       <h2>story</h2>
       <p>
-        i was so curious that my mom had to teach me how to read and write when I was <strong>3</strong>.
-        i loved tinkering with things, to understand how they work and how to build them.
-        <strong>i've always been dreaming to be an inventor since then.</strong>
-        <br>
-        this was the start in my life.
+        <span class="paragraph-tagline">👶🏻</span>
+        i was a <i>curious</i> and <i>artistic</i> child, and my mom did teach me how to read and write when I was
+        <strong>3</strong>. i loved tinkering with gadgets, to understand how they work and how to build my own things.
       </p>
-      <h4>a</h4>
-      <p>When I started elementary school, I spent my whole time with <strong>reading about history and drawing the
-          moments, battle scenes, maps which I just read.</strong> This was exciting until I realized that how tables
-        can turn so quickly. Today I may be victorious, glorious; but tomorrow maybe defeated, perished.</p>
+      <p>
+        <strong>i've always been dreaming to be an inventor since then.</strong>
+        <br>this was the start in my life.
+      </p>
+      <p>
+        <span class="paragraph-tagline">🏫</span>
+        when i started elementary school, i spent my whole time <b>reading about history</b> and
+        <b>picturing the moments</b>, battle scenes, maps which I just read.
+        this was exciting until i realized that how tables can turn so quickly.
+      </p>
+      <Quote message="
+        For the loser now
+      / Will be later to win
+      / For the times they are a-changin'" owner="bob dylan" />
       <h3>New beginnings...</h3>
       <p>I started programming when I was <strong>10</strong>, and this was a milestone in my journey. You write the
-        code, and let everyone use it. What you can do is only limited by your skills and your imagination. My secondary
+        code, and let everyone use it. What you can do is only limited by your skills and your imagination. My
+        secondary
         school years were spent on personal projects.</p>
-      <p>After a boring exam year, I entered <strong>Vefa Lisesi</strong>, a historical high school in Istanbul. Then I
+      <p>After a boring exam year, I entered <strong>Vefa Lisesi</strong>, a historical high school in Istanbul. Then
+        I
         discovered <strong>the Web</strong>, and started writing for it. Incredible. There also I met Berk, who is a
         fellow of <a href="https://dorkodu.com">Dorkodu</a>.</p>
       <h3>Dorkodu: Revolutions</h3>
-      <p>Dorkodu was born when I first branded all my work under the name of "Dorkodu", I was in secondary school then.
+      <p>Dorkodu was born when I first branded all my work under the name of "Dorkodu", I was in secondary school
+        then.
         The name comes from <strong>"Doruk" + "code"</strong>.</p>
-      <p>When I saw the truth behind the Web, I asked this question myself : <strong>Do we really get the pure and true
+      <p>When I saw the truth behind the Web, I asked this question myself : <strong>Do we really get the pure and
+          true
           information from Web? No. All we get is some pages that others created.</strong> There is no chance to get
         what/how exactly you want.</p>
-      <p>What if we had a <strong>universal knowledge resource</strong> that we can find, discover and exchange only the
+      <p>What if we had a <strong>universal knowledge resource</strong> that we can find, discover and exchange only
+        the
         true, meaningful and useful information? The search results would be pure information pieces connected to each
         other, gathered from the whole accumilation of open human knowledge.</p>
       <p>This is what I dedicated the first part of my career to.</p>
